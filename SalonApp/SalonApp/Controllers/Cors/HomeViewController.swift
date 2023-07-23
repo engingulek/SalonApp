2aa1f64aef0f9452fe3395a37943da7698021549
Engin Gülek <engingulek0@gmail.com>
2023-07-23 11:13:03 +0300

    HomePage View Collection View changed
    Gender collection view deleted and service name changed to top services

--- SalonApp/SalonApp/Controllers/Cors/HomeViewController.swift
+++ SalonApp/SalonApp/Controllers/Cors/HomeViewController.swift
@@ -8,11 +8,7 @@
 import UIKit
 import SnapKit
 
-enum Section : Int {
-    case Gender
-    case Service
 
-}
 
 
 class HomeViewController: UIViewController {
@@ -31,35 +27,10 @@ class HomeViewController: UIViewController {
  
         return textField
     }()
-    
-    
-    private let genderTitleLabel : UILabel = {
-        let label = UILabel()
-        label.text = "Gender"
-        label.textColor = .black
-        label.font = .systemFont(ofSize: 18,weight: .bold)
-        return label
-    }()
-    
-    
-    private let genderCollectionView  : UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 0)
-        layout.minimumLineSpacing = 20
-        layout.minimumInteritemSpacing = 10
-        layout.itemSize = CGSize(width: 80, height: 50)
-        let collectionView = UICollectionView(frame: .infinite,collectionViewLayout: layout)
-        collectionView.register(GenderCollectionViewCell.self, forCellWithReuseIdentifier: GenderCollectionViewCell.identifier)
-        collectionView.backgroundColor = UIColor(named: "backColor")
-        
-        return collectionView
-    }()
-    
-    
+
     private let serviceTitleLabel : UILabel = {
         let label = UILabel()
-        label.text = "Services"
+        label.text = "Top Services"
         label.textColor = .black
         label.font = .systemFont(ofSize: 18,weight: .bold)
         return label
@@ -86,13 +57,11 @@ class HomeViewController: UIViewController {
         view.backgroundColor = UIColor(named: "backColor")
         view.addSubview(headerView)
         view.addSubview(searchTextFeield)
-        view.addSubview(genderTitleLabel)
-        view.addSubview(genderCollectionView)
+      
         view.addSubview(serviceTitleLabel)
         view.addSubview(serviceCollectionView)
         configureConstraints()
-        genderCollectionView.delegate = self
-        genderCollectionView.dataSource = self
+        
         serviceCollectionView.delegate = self
         serviceCollectionView.dataSource = self
         
@@ -106,27 +75,16 @@ class HomeViewController: UIViewController {
             make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing).offset(-10)
         }
         
-        genderTitleLabel.snp.makeConstraints { make in
+       
+        
+        serviceTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(searchTextFeield.snp.bottom).offset(20)
             make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(15)
             
         }
         
-        genderCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(genderTitleLabel.snp.bottom).offset(3)
-            make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
-            make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
-            make.height.equalTo(80)
-        }
-        
-        serviceTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(genderCollectionView.snp.bottom).offset(20)
-            make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(15)
-            
-        }
-        
         serviceCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(serviceTitleLabel.snp.bottom).offset(3)
+            make.top.equalTo(serviceTitleLabel.snp.bottom).offset(10)
             make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
             make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
             make.height.equalTo(80)
@@ -142,18 +100,9 @@ extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSour
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       
         
-        if collectionView == self.genderCollectionView {
-            guard let cell =  genderCollectionView.dequeueReusableCell(withReuseIdentifier: GenderCollectionViewCell.identifier,
-                                                                       for: indexPath) as? GenderCollectionViewCell else {
-                return UICollectionViewCell()
-            }
-            cell.genderLabel.text = "Man"
-            cell.backgroundColor = .white
-            cell.layer.cornerRadius = 20
-            return cell
-        }
+       
         
-        else if collectionView == self.serviceCollectionView {
+         if collectionView == self.serviceCollectionView {
             guard let cell =  serviceCollectionView.dequeueReusableCell(withReuseIdentifier: ServiceCollectionViewCell.identifier,
                                                                        for: indexPath) as? ServiceCollectionViewCell else {
                 return UICollectionViewCell()
